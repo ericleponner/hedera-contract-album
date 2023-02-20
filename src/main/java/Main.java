49fff9af2edcaae79d3d60b5557f426dbc17bc4a
Eric Le Ponner <eric.leponner@icloud.com>
2023-02-20 19:07:13 +0100

    Added top Main class.

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import hello_world.HelloWorld;
+import hts.HTSv2;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        HelloWorld.deploy();
+        HTSv2.deploy();
+    }
+}
