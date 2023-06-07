2cc3a6258f5f798973303b3cce2ae775dbe0fae0
Eric Le Ponner <eric.leponner@icloud.com>
2023-06-07 15:01:41 +0200

    Updated Main class with latest contracts.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,16 @@
+import hello_swarm.HelloSwarm;
 import hello_world.HelloWorld;
 import hts.HTSv2;
 import test_error.TestError;
+import test_event.TestEvent;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        HelloSwarm.deploy();
         HelloWorld.deploy();
         HTSv2.deploy();
         TestError.deploy();
+        TestEvent.deploy();
     }
 }
