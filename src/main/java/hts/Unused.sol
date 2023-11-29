e90bfbde0c2b135ddecf648266d484a713139cf3
Eric Le Ponner <eric.leponner@icloud.com>
2023-11-29 08:37:45 +0100

    Added Unused.sol in hts contract.

---
+++ src/main/java/hts/Unused.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.17;
+
+import "./HederaTokenService.sol";
+import "./HederaResponseCodes.sol";
+
+
+contract HTS {
+
+    function dummy() public {
+
+    }
+}
+
+
+contract HelloWorld {
+
+    function dummy() public {
+
+    }
+
+}
+
